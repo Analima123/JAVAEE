43bcd9d0571905f19566c92eb068fbf02fdde4f9
Aluno <Aluno@DSN1251230721>
2022-06-06 16:17:44 -0300

    PROJETO INVERSÃO DE TEXTO

---
+++ Inverter/src/main/java/br/com/analima/application/FormBean.java
@@ -0,0 +1,36 @@
+package br.com.analima.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class FormBean implements Serializable {
+
+	private String texto;
+	private String textoInvertido;
+	
+	public String inverter() {
+		// Inverte o texto e direciona o usus�rio pra pr�pria tela onde ele estava
+		textoInvertido = new StringBuilder(texto).reverse().toString();
+		return null;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getTextoInvertido() {
+		return textoInvertido;
+	}
+
+	public void setTextoInvertido(String textoInvertido) {
+		this.textoInvertido = textoInvertido;
+	}
+}

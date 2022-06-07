0d138b4deeef5538161dbc293ad6642794be4974
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-07 14:09:17 -0300

    Update PageBean.java

--- ProjetoNavegacaoTelas/src/main/java/br/com/senai/analima/application/PageBean.java
+++ ProjetoNavegacaoTelas/src/main/java/br/com/senai/analima/application/PageBean.java
@@ -4,12 +4,20 @@ import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+// Usado para retirar o Warning na tela 
 @SuppressWarnings("serial")
+
+
+// Define o nome que o BEAN será chamado na página XHTML
 @Named
+
+
+// Define o tempo de vida do ESCOPO, no caso o REQUESTSCOPED ficará "vivo" durante a requisição HTTP, ou seja, após a mostra do resultado,tudo será excluido e será gerada uma nova requisição caso a página seja acionada novamente.
 @RequestScoped
 
 
 
+// A serialização significa salvar o estado atual dos objetos em arquivos em formato binário para o seu computador, sendo assim esse estado poderá ser recuperado posteriormente recriando o objeto em memória assim como ele estava no momento da sua serialização.
 public class PageBean implements Serializable {
 	
 	
@@ -25,7 +33,8 @@ public class PageBean implements Serializable {
 		this.page = page;
 	}
 	
-	
+
+// Se a informação escolhida for igual ao nome da página, ela será retornada	
 public String goTo() {
 	
 	if ("a".equals(page)) {
@@ -33,13 +42,13 @@ public String goTo() {
 		return "page_a";
 		
 	
-	
+// Se a informação escolhida for igual ao nome da página, ela será retornada		
 }else if ("b".equals(page)) {
 
 	return "page_b";
 	
 
-
+//Recarrega a mesma página
 }else {
 	return null;
 

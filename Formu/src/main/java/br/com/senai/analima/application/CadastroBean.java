1ca2d5895ce96287d658a883ff484b9226fca833
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-06 22:38:09 -0300

    Update CadastroBean.java

--- Formu/src/main/java/br/com/senai/analima/application/CadastroBean.java
+++ Formu/src/main/java/br/com/senai/analima/application/CadastroBean.java
@@ -14,13 +14,18 @@ import br.com.senai.analima.model.Endereco;
 import br.com.senai.analima.model.Pessoa;
 import br.com.senai.analima.model.Telefone;
 
-
+// Define o nome que o BEAN será chamado na página XHTML, no caso o nome será "cadastro"
 @Named("cadastro")
+
+// O Escopo ficará vivo enquanto a sessão estiver viva. A sessão ficará viva enquanto o navegador não for fechado ou ao ter um time out definido
 @SessionScoped
+
+// A serialização significa salvar o estado atual dos objetos em arquivos em formato binário para o seu computador, sendo assim esse estado poderá ser recuperado posteriormente recriando o objeto em memória assim como ele estava no momento da sua serialização.
 public class CadastroBean implements Serializable {
 
 	private Pessoa pessoa;
 
+	// Pega os atributos do objeto pessoa, se o objeto "pessoa" ainda não existir, será criado e retornado
 	public Pessoa getPessoa() {
 		if (pessoa == null) {
 			pessoa = new Pessoa();
@@ -35,14 +40,16 @@ public class CadastroBean implements Serializable {
 		return pessoa;
 	}
 
+	// Seta o objeto "pessoa" dentro de Pessoa 
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
-	
+	// Pega os atributos e retorna a lista de ESTADOS
 	public List<Estado> getEstados() {
 		return Lista.ESTADOS;
 	}
-
+	
+	// Pega os atributos e retorna a lista de INTERESSES
 	public List<Interesse> getInteresses() {
 		return Lista.INTERESSES;
 	}

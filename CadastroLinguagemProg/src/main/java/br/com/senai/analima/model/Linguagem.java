c8e00262730777ce617fdb4c3bfc4619ceb761ca
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-07 21:55:25 -0300

    Update Linguagem.java

--- CadastroLinguagemProg/src/main/java/br/com/senai/analima/model/Linguagem.java
+++ CadastroLinguagemProg/src/main/java/br/com/senai/analima/model/Linguagem.java
@@ -6,6 +6,12 @@ public class Linguagem {
 	private Integer id;
 	private String nome;
 	
+// Uma constante é a alocação de uma espaço na memória para guardar um valor durante toda a execução de um programa ou então, durante a execução de um determinado Bloco de Instrução.
+// A diferença entre CONSTANTE e VARIÁVEL, é que em nenhum momento a constante pode sofrer alteração de valor durante o seu tempo de execução
+// Deve ser chamada com todas as letras MAIÚSCULAS, como no exemplo abaixo 
+// Uma classe com o modificador "final" não pode ser estendida,não pode ter classes que herdam dela.
+// [Nº] Representa a posição 
+	
 	public static final Linguagem[] LINGUAGENS;
 	
 	static {

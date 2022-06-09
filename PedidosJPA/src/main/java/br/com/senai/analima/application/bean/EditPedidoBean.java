08d018b0a219b747000e8bd883f8e7001dae90c1
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-08 22:48:57 -0300

    Update EditPedidoBean.java

--- PedidosJPA/src/main/java/br/com/senai/analima/application/bean/EditPedidoBean.java
+++ PedidosJPA/src/main/java/br/com/senai/analima/application/bean/EditPedidoBean.java
@@ -14,11 +14,18 @@ import br.com.senai.analima.application.ejb.ProdutoBean;
 import br.com.senai.analima.application.model.Cliente;
 import br.com.senai.analima.application.model.Produto;
 
-
+// Define o nome que o BEAN será chamado na página XHTML
 @Named
+
+// Define o tempo de vida do ESCOPO, no caso o REQUESTSCOPED ficará "vivo" durante a requisição HTTP, ou seja, após a mostra do resultado,tudo será excluido e será gerada uma nova requisição caso a página seja acionada novamente.
 @RequestScoped
+
+
+// A serialização significa salvar o estado atual dos objetos em arquivos em formato binário para o seu computador, sendo assim esse estado poderá ser recuperado posteriormente recriando o objeto em memória assim como ele estava no momento da sua serialização.
 public class EditPedidoBean implements Serializable {
 	
+//Lógica de negócios	
+//EJB é um framework do Java EE. Eles são executados em um container EJB no lado do servidor. A principal característica do EJB é a rapidez em desenvolver soluções, pois, ele fornece um conjunto de componentes prontos, o que torna o desenvolvimento eficiente.
 	@EJB
 	private PedidoBean pedidoBean;
 	
@@ -34,6 +41,9 @@ public class EditPedidoBean implements Serializable {
 	private Integer selectedClienteId;
 	private Integer[] selectedProdutosIds;
 	
+	
+	// No @PostConstruct método, o bean é totalmente inicializado e você pode usar as dependências.
+	// Precisa ser executado após a injeção de dependência ser executada para executar qualquer inicialização.
 	@PostConstruct
 	public void init() {
 		clientes = clienteBean.listar();

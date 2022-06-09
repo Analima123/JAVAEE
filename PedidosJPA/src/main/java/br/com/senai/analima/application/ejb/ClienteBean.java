3a9aee38ee96f06c874d01930152e42cd087091d
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-08 23:09:18 -0300

    Update ClienteBean.java

--- PedidosJPA/src/main/java/br/com/senai/analima/application/ejb/ClienteBean.java
+++ PedidosJPA/src/main/java/br/com/senai/analima/application/ejb/ClienteBean.java
@@ -8,13 +8,19 @@ import javax.persistence.PersistenceContext;
 
 import br.com.senai.analima.application.model.Cliente;
 
-
+// Utilizar Stateless quando não há necessidade de manter o estado dos valores
+// Como a lista de produtos sempre será a mesma independete do cliente, usamos o Stateless
 @Stateless
+
 public class ClienteBean {
 
 	@PersistenceContext
-	private EntityManager em;
 	
+	//Recurso responsável por realizar as operações de sincronismo com o banco de dados (inserir, remover, atualizar ou consultar - CRUD) e gerenciar o ciclo de vida das entidades.
+	//Quando uma inserção no banco de dados é realizada, o EntityManager será o responsável
+	
+	
+	//Método Listar Clientes
 	public List<Cliente> listar() {
 		return em.createQuery("SELECT c FROM Cliente c", Cliente.class).getResultList();
 	}

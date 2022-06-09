a50ec6f989a973b17e9c5492cf7b7a915c1c79e0
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-08 23:21:23 -0300

    Update PedidoBean.java

--- PedidosJPA/src/main/java/br/com/senai/analima/application/ejb/PedidoBean.java
+++ PedidosJPA/src/main/java/br/com/senai/analima/application/ejb/PedidoBean.java
@@ -14,16 +14,31 @@ import br.com.senai.analima.application.model.Produto;
 import br.com.senai.analima.application.model.Pagamento.TipoPagamento;
 
 
+// Utilizar Stateless quando não há necessidade de manter o estado dos valores
+// Como a lista de produtos sempre será a mesma independete do cliente, usamos o Stateless
+
 @Stateless
+
 public class PedidoBean {
 
+	
+	//Recurso responsável por realizar as operações de sincronismo com o banco de dados (inserir, remover, atualizar ou consultar - CRUD) e gerenciar o ciclo de vida das entidades.
+	//Quando uma inserção no banco de dados é realizada, o EntityManager será o responsável
+	
 	@PersistenceContext
+	
 	private EntityManager em;
 	
+	// Método listar pedidos
+	// Os valores de pedido serão retirados do banco de dados
 	public List<Pedido> listar() {
 		return em.createQuery("SELECT p FROM Pedido p", Pedido.class).getResultList();
 	}
 	
+	// Método pagar
+	// em.find --> Faz a procura diretamente no banco de dados
+	// em.persist --> A persistência dos dados tem o objetivo de garantir que as informações serão armazenadas em um meio em que possam ser recuperadas de forma consistente
+
 	public void pagar(Integer pedidoId, String tipo) {
 		TipoPagamento tipoPagamento = TipoPagamento.valueOf(tipo);
 		Pedido pedido = em.find(Pedido.class, pedidoId);
@@ -35,11 +50,13 @@ public class PedidoBean {
 		pedido.setPagamento(pagamento);
 	}
 	
+		// Método excluir
 	public void excluir(Integer pedidoId) {
 		Pedido pedido = em.find(Pedido.class, pedidoId);
 		em.remove(pedido);
 	}
 	
+		// Método cadastrar
 	public void cadastrar(Integer clienteId, Integer[] produtosIds) throws Exception {
 		Cliente cliente = em.find(Cliente.class, clienteId);
 		

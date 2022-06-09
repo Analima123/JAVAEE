f30ff90d7a760bc515497f30034638139b495289
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-08 23:32:55 -0300

    Update Cliente.java

--- PedidosJPA/src/main/java/br/com/senai/analima/application/model/Cliente.java
+++ PedidosJPA/src/main/java/br/com/senai/analima/application/model/Cliente.java
@@ -11,20 +11,33 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+
+// Usado para retirar o Warning na tela 
 @SuppressWarnings("serial")
+
+
+// As classes Java que representam os dados a serem armazenados no SGBD são chamadas de Entidades (Entity).
 @Entity
+
+// A serialização significa salvar o estado atual dos objetos em arquivos em formato binário para o seu computador, sendo assim esse estado poderá ser recuperado posteriormente recriando o objeto em memória assim como ele estava no momento da sua serialização.
 public class Cliente implements Serializable {
 	
+	// Define a primary key da tabela
 	@Id
+	
+	// JPA passará a utilizar a geração de chave primária do banco de dados ao qual a aplicação está conectada.
 	@GeneratedValue
 	private Integer id;
 	
+	// Define o tamanho da coluna e se ela será "not null"
 	@Column(length = 100, nullable = false)
 	private String nome;
 	
 	@Column(length = 50, nullable = false)
 	private String email;
 
+	// Relacionamento de cardinalidade entre a tabela cliente e pedidos
+	// Um para muitos
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos;
 

e8f4cf8ac078502aed3405303f98e396106b37a6
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-08 23:43:30 -0300

    Update Pagamento.java

--- PedidosJPA/src/main/java/br/com/senai/analima/application/model/Pagamento.java
+++ PedidosJPA/src/main/java/br/com/senai/analima/application/model/Pagamento.java
@@ -11,24 +11,43 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 
+	
+// As classes Java que representam os dados a serem armazenados no SGBD são chamadas de Entidades (Entity).
+@Entity
+
+// A serialização significa salvar o estado atual dos objetos em arquivos em formato binário para o seu computador, sendo assim esse estado poderá ser recuperado posteriormente recriando o objeto em memória assim como ele estava no momento da sua serialização.
+public class Pagamento implements Serializable {
+	
 	// Uma enum é um tipo no qual declaramos um conjunto de valores constantes pré-definidos.
 	// Por serem constantes, possuem as letras MAIÚSCULAS
 	// Ao utilizar enums, limitamos os valores que podem ser atribuídos a uma variável. Sendo assim, devemos atribuir ao campo um dos valores pré-definidos na enum TipoPagamento.
-@Entity
-public class Pagamento implements Serializable {
+	
 	public enum TipoPagamento {
 		CARTAO_CREDITO,
 		BOLETO
 	}
 
+	// Primary key da tabela
 	@Id
+	
+	// JPA passará a utilizar a geração de chave primária do banco de dados ao qual a aplicação está conectada.
 	@GeneratedValue
+	
 	private Integer id;
 	
+	// É usada para especificar que um atributo de entidade representa um tipo enumerado.
+	
+	// @Enumerated(EnumType.ORDINAL) → Armazenar os valores de enum de acordo com a posição ordinal (ou seja, 0 , 1, 2 … ) do valor de enum.
+	// @Enumerated(EnumType.STRING) → Armazenar os valores enum de acordo com o nome do valor enum. O padrão é EnumType.ORDINAL.
+	
 	@Enumerated(EnumType.STRING)
+	
+	// Define o tamanho da coluna e se ela será "not null"
 	@Column(name = "tipo_pagto", length = 20, nullable = false)
 	private TipoPagamento tipoPagto;
 	
+	
+	// Relacionamento de cardinalidade um para um 
 	@OneToOne(mappedBy = "pagamento")
 	private Pedido pedido;
 	

986e8d1f0c8081c51a09ce5ddda60c99fd1e5610
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-08 23:24:45 -0300

    Update ProdutoBean.java

--- PedidosJPA/src/main/java/br/com/senai/analima/application/ejb/ProdutoBean.java
+++ PedidosJPA/src/main/java/br/com/senai/analima/application/ejb/ProdutoBean.java
@@ -8,9 +8,18 @@ import javax.persistence.PersistenceContext;
 
 import br.com.senai.analima.application.model.Produto;
 
+
+// São criados apenas uma vez, e cada objeto que depende da classe compartilha a mesma instância
+// Útil para compartilhar contexto em todo o aplicativo ou como otimização de desempenho
+
 @Stateless
+
+
+
 public class ProdutoBean {
 
+	// Recurso responsável por realizar as operações de sincronismo com o banco de dados (inserir, remover, atualizar ou consultar - CRUD) e gerenciar o ciclo de vida das entidades.
+	// Quando uma inserção no banco de dados é realizada, o EntityManager será o responsável
 	@PersistenceContext
 	private EntityManager em;
 	

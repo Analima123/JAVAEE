b75049e5aeeb0070666989c0274c92bdda3fab4f
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-08 22:53:06 -0300

    Update FormatterBean.java

--- PedidosJPA/src/main/java/br/com/senai/analima/application/bean/FormatterBean.java
+++ PedidosJPA/src/main/java/br/com/senai/analima/application/bean/FormatterBean.java
@@ -8,10 +8,18 @@ import java.util.Locale;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
+// Define o nome que o BEAN será chamado na página XHTML
 @Named
+
+// São criados apenas uma vez, e cada objeto que depende da classe compartilha a mesma instância
+// Útil para compartilhar contexto em todo o aplicativo ou como otimização de desempenho
 @ApplicationScoped
+
+
+// A serialização significa salvar o estado atual dos objetos em arquivos em formato binário para o seu computador, sendo assim esse estado poderá ser recuperado posteriormente recriando o objeto em memória assim como ele estava no momento da sua serialização.
 public class FormatterBean implements Serializable {
 
+	//Método utilizado para transformar os valores em moeda brasileira
 	private static final Locale LOCALE_BR = new Locale("pt", "BR");
 	
 	public String formatarMoeda(double valor) {

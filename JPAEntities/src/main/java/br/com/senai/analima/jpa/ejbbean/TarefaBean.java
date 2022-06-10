8ac49dced2d1a6b5f32246d2fd265ed2d5fcc9ee
Analima123 <102822383+Analima123@users.noreply.github.com>
2022-06-10 14:05:13 -0300

    Update TarefaBean.java

--- JPAEntities/src/main/java/br/com/senai/analima/jpa/ejbbean/TarefaBean.java
+++ JPAEntities/src/main/java/br/com/senai/analima/jpa/ejbbean/TarefaBean.java
@@ -7,30 +7,34 @@ import javax.persistence.PersistenceContext;
 import br.com.senai.analima.model.Tarefa;
 
 
-
+// Utilizar Stateless quando não há necessidade de manter o estado dos valores
 @Stateless
 
 
 public class TarefaBean {
 	
+// Recurso responsável por realizar as operações de sincronismo com o banco de dados (inserir, remover, atualizar ou consultar - CRUD) e gerenciar o ciclo de vida das entidades.
+// Quando uma inserção no banco de dados é realizada, o EntityManager será o responsável
 	
-//Persistence context é onde fica armazenado os objetos do entity manager
 	@PersistenceContext
+	
+	
 	private EntityManager em;
-	//Persist coloca os dados no banco de dados
+	
+	//Persist insere as tarefas no banco de dados
 	public void inserir(Tarefa tarefa) {
 		em.persist(tarefa);
 	}
-	//merge sobrescreve os dados
+	// Merge sobrescreve os dados de "tarefa"
 	public void atualizar(Tarefa tarefa) {
 		em.merge(tarefa);
 	}
-	//exclui os dados
+	// Exclui os dados de "tarefa"
 	public void excluir(Tarefa tarefa) {
 		tarefa = carregar(tarefa.getId());
 		em.remove(tarefa);
 	}
-	
+	// Carrega as tarefas e logo depois as procura pelo ID
 	public Tarefa carregar(int id) {
 		//find procura o valor
 		return em.find(Tarefa.class, id);
